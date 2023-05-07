3ed59a8e705a8e2f8a9cf7d460964651a0cafa20
Ruslan Asabidi <ruslanasabidi@gmail.com>
2023-05-07 15:07:43 +0200

    sound_manager class, vocal range enum and background picture and sound
    sound_manager: handles all sound functionalities
    
    vocal range enum: handles test states

---
+++ karaoke-game/sound_manager.py
@@ -0,0 +1,46 @@
+class Sound_Manager():
+
+    def __init__(self, low_notes, high_notes, notes_index, note_input, lowest_achieved_note, highest_achieved_note):
+        self.low_notes = low_notes
+        self.high_notes = high_notes
+        self.notes_index = notes_index
+        self.note_input = note_input
+        self.lowest_achieved_note = lowest_achieved_note
+        self.highest_achieved_note = highest_achieved_note
+        self.notes_index = notes_index
+
+    def get_notes_index(self):
+        return self.notes_index
+    
+    def get_low_notes_arr(self):
+        return self.low_notes
+    
+    def get_high_notes_arr(self):
+        return self.high_notes
+
+    def set_notes_index(self, new_index):
+        self.notes_index = new_index
+
+    def get_next_low_note(self):
+        return self.low_notes[self.notes_index]
+    
+    def get_next_high_note(self):
+        return self.high_notes[self.notes_index]
+    
+    def set_note_input(self, new_note_input):
+        self.note_input = new_note_input
+
+    def get_note_input(self):
+        return self.note_input
+    
+    def set_lowest_achieved_note(self, new_note):
+        self.lowest_achieved_note = new_note
+
+    def set_highest_achieved_note(self, new_note):
+        self.highest_achieved_note = new_note
+
+    def get_lowest_achieved_note(self):
+        return self.lowest_achieved_note
+    
+    def get_highest_achieved_note(self):
+        return self.highest_achieved_note
---
+++ karaoke-game/vocal_range_enum.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Vocal_Range(Enum):
+    DESCRIPTION = 0
+    LOW_TEST = 1
+    HIGH_TEST = 2
+    LOW_TESTED = 3
+    HIGH_TESTED = 4
